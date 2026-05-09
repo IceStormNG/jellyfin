43b3a4342a620303e402ca40be9f664815060434
IceStormNG <carstenbraun@live.de>
2026-05-09 16:40:07 +0200

    Trickplay parallel generation

--- MediaBrowser.Providers/Trickplay/TrickplayImagesTask.cs
+++ MediaBrowser.Providers/Trickplay/TrickplayImagesTask.cs
@@ -92,24 +92,24 @@ public class TrickplayImagesTask : IScheduledTask
             query.StartIndex = startIndex;
             var videos = _libraryManager.GetItemList(query).OfType<Video>();
 
-            foreach (var video in videos)
-            {
-                cancellationToken.ThrowIfCancellationRequested();
+            await Parallel.ForEachAsync(
+               videos,
+               new ParallelOptions() { CancellationToken = cancellationToken },
+               async (video, ct) =>
+               {
+                   try
+                   {
+                       var libraryOptions = _libraryManager.GetLibraryOptions(video);
+                       await _trickplayManager.RefreshTrickplayDataAsync(video, false, libraryOptions, ct).ConfigureAwait(false);
+                   }
+                   catch (Exception ex)
+                   {
+                       _logger.LogError(ex, "Error creating trickplay files for {ItemName}", video.Name);
+                   }
 
-                try
-                {
-                    var libraryOptions = _libraryManager.GetLibraryOptions(video);
-                    await _trickplayManager.RefreshTrickplayDataAsync(video, false, libraryOptions, cancellationToken).ConfigureAwait(false);
-                }
-                catch (Exception ex)
-                {
-                    _logger.LogError(ex, "Error creating trickplay files for {ItemName}", video.Name);
-                }
-
-                numComplete++;
-                progress.Report(100d * numComplete / numberOfVideos);
-            }
-
+                   var completed = Interlocked.Increment(ref numComplete);
+                   progress.Report(100d * completed / numberOfVideos);
+               }).ConfigureAwait(false);
             startIndex += QueryPageLimit;
         }
 
